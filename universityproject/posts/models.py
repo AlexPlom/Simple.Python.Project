c5c6c759019aa837b705c1d9aa2bf1f6ba472569
AlexPlom <alexiosplomaritis@gmail.com>
2018-05-19 19:38:39 +0300

    Does most of the work

--- universityproject/posts/models.py
+++ universityproject/posts/models.py
@@ -6,3 +6,9 @@ class Posts(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField(max_length=2000)
     createdAt = models.DateTimeField(default=datetime.now,blank=True)
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Posts"
