21df5fb67940e93f9b6cd5c1819857711ece6480
AlexPlom <alexiosplomaritis@gmail.com>
2018-06-05 01:05:39 +0300

    Adds more functionality

--- universityproject/posts/views.py
+++ universityproject/posts/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Posts
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required(login_url="/login/")
 def index(request):
-
     posts = Posts.objects.all()[:10]
     
     context  = {
